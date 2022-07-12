d59227f9a69d197c7cbba6110caea7c69b9b5e61
harrison314 <harrison314@azet.sk>
2022-07-12 17:43:21 +0200

    Add missing files.

---
+++ src/src/Area52/Shared/Logs/SerachControlContext.cs
@@ -0,0 +1,51 @@
+﻿using Area52.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area52.Shared.Logs;
+
+public delegate void QueryChangeDelegate(string queryApendix, bool replace, bool exceute);
+
+public class SerachControlContext
+{
+    public event QueryChangeDelegate? OnQueryChange;
+
+    public SerachControlContext()
+    {
+
+    }
+
+    public void AddToQuery(LogEntityProperty property)
+    {
+        this.OnQueryChange?.Invoke(this.ToQuery(property), false, false);
+    }
+
+    public void SearchNow(LogEntityProperty property)
+    {
+        this.OnQueryChange?.Invoke(this.ToQuery(property), true, true);
+    }
+
+    private string ToQuery(LogEntityProperty property)
+    {
+        if (property.Valued.HasValue)
+        {
+            return string.Format(System.Globalization.CultureInfo.InvariantCulture,
+                "{0} is {1}",
+                property.Name,
+                property.Valued.Value);
+        }
+
+        if (property.Values != null)
+        {
+            return string.Concat(property.Name,
+                " is '",
+                property.Values.Replace("'", "\\'").Replace("\n", "\\n"),
+                "'");
+        }
+
+        throw new InvalidProgramException("Missing value of LogEntityProperty.");
+    }
+}
