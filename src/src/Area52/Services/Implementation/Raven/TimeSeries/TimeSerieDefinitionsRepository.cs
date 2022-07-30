1011de10ae5d00bbf057bbfb471aaf31d3632487
harrison314 <harrison314@azet.sk>
2022-07-30 14:34:38 +0200

    Add better Error handling in timeseries.

--- src/src/Area52/Services/Implementation/Raven/TimeSeries/TimeSerieDefinitionsRepository.cs
+++ src/src/Area52/Services/Implementation/Raven/TimeSeries/TimeSerieDefinitionsRepository.cs
@@ -23,32 +23,57 @@ public class TimeSerieDefinitionsRepository : ITimeSerieDefinitionsRepository
     {
         this.logger.LogTrace("Entering to Create.");
 
-        using var session = this.documentStore.OpenAsyncSession();
+        try
+        {
+            using var session = this.documentStore.OpenAsyncSession();
 
-        await session.StoreAsync(timeSerieDefinition);
-        await session.SaveChangesAsync();
+            await session.StoreAsync(timeSerieDefinition);
+            await session.SaveChangesAsync();
 
-        return timeSerieDefinition.Id;
+            return timeSerieDefinition.Id;
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in Create method.");
+            throw;
+        }
     }
 
     public async Task<TimeSerieDefinition> FindById(string id)
     {
         this.logger.LogTrace("Entering to FindById with id {id}.", id);
 
-        using var session = this.documentStore.OpenAsyncSession();
-        return await session.LoadAsync<TimeSerieDefinition>(id);
+        try
+        {
+            using var session = this.documentStore.OpenAsyncSession();
+            return await session.LoadAsync<TimeSerieDefinition>(id);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in FindById method with id {id}.", id);
+            throw;
+        }
     }
 
     public async Task<IReadOnlyList<TimeSerieDefinitionInfo>> FindDefinictions()
     {
         this.logger.LogTrace("Entering to Create.");
 
-        using var session = this.documentStore.OpenAsyncSession();
-        return await session.Query<TimeSerieDefinition>().Select(t => new TimeSerieDefinitionInfo()
+        try
         {
-            Id = t.Id,
-            Name = t.Name,
-            Description = t.Description
-        }).ToListAsync();
+            using var session = this.documentStore.OpenAsyncSession();
+            return await session.Query<TimeSerieDefinition>().Select(t => new TimeSerieDefinitionInfo()
+            {
+                Id = t.Id,
+                Name = t.Name,
+                Description = t.Description
+            }).ToListAsync();
+
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in FindDefinictions method.");
+            throw;
+        }
     }
 }
--- src/src/Area52/Services/Implementation/Raven/TimeSeries/TimeSeriesService.cs
+++ src/src/Area52/Services/Implementation/Raven/TimeSeries/TimeSeriesService.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Area52.Services.Contracts.TimeSeries;
 using Area52.Services.Implementation.Raven.Indexes;
-using Area52.Services.Implementation.Raven.Models;
 using Raven.Client.Documents;
 using Raven.Client.Documents.Queries.TimeSeries;
 
@@ -28,90 +27,130 @@ public class TimeSeriesService : ITimeSeriesService
     {
         this.logger.LogTrace("Entering to GetDefinitionForExecute with executeBefore={executeBefore}.", executeBefore);
 
-        using var session = this.documentStore.OpenAsyncSession();
+        try
+        {
+            using var session = this.documentStore.OpenAsyncSession();
 
-        var ids = await session.Query<TimeSerieDefinitionIndex.Result, TimeSerieDefinitionIndex>().Where(t => t.LastExecute == null || t.LastExecute < executeBefore)
-             .Select(t => t.Id)
-             .ToListAsync(cancellationToken);
+            List<string> ids = await session.Query<TimeSerieDefinitionIndex.Result, TimeSerieDefinitionIndex>().Where(t => t.LastExecute == null || t.LastExecute < executeBefore)
+                 .Select(t => t.Id)
+                 .ToListAsync(cancellationToken);
 
-        var result = new List<TimeSeriesDefinitionId>(ids.Count);
-        result.AddRange(ids.Select(t => new TimeSeriesDefinitionId(t)));
+            var result = new List<TimeSeriesDefinitionId>(ids.Count);
+            result.AddRange(ids.Select(t => new TimeSeriesDefinitionId(t)));
 
-        return result;
+            return result;
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in GetDefinitionForExecute with executeBefore {executeBefore}.", executeBefore);
+            throw;
+        }
     }
 
     public async Task<TimeSeriesDefinitionUnit> GetDefinitionUnit(string id, CancellationToken cancellationToken)
     {
         this.logger.LogTrace("Entering to GetDefinitionUnit with id={id}.", id);
 
-        using var session = this.documentStore.OpenAsyncSession();
+        try
+        {
+            using var session = this.documentStore.OpenAsyncSession();
 
-        TimeSerieDefinition definition = await session.LoadAsync<TimeSerieDefinition>(id, cancellationToken);
-        return new TimeSeriesDefinitionUnit(definition.Id,
-            definition.Name,
-            definition.Query,
-            definition.ValueFieldName,
-            definition.TagFieldName,
-            definition?.LastExecutionInfo?.LastExecute);
+            TimeSerieDefinition definition = await session.LoadAsync<TimeSerieDefinition>(id, cancellationToken);
+            return new TimeSeriesDefinitionUnit(definition.Id,
+                definition.Name,
+                definition.Query,
+                definition.ValueFieldName,
+                definition.TagFieldName,
+                definition?.LastExecutionInfo?.LastExecute);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in GetDefinitionUnit method with id {id}.", id);
+            throw;
+        }
     }
 
     public Task<ITimeSeriesWriter> CreateWriter(string definitionId, CancellationToken cancellationToken)
     {
         this.logger.LogTrace("Entering to CreateWriter with definitionId={definitionId}.", definitionId);
 
-        return Task.FromResult<ITimeSeriesWriter>(new TimeSeriesWriter(this.documentStore,
-            definitionId,
-            this.loggerFactory.CreateLogger<TimeSeriesWriter>()));
+        try
+        {
+            return Task.FromResult<ITimeSeriesWriter>(new TimeSeriesWriter(this.documentStore,
+                definitionId,
+                this.loggerFactory.CreateLogger<TimeSeriesWriter>()));
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in CreateWriter method with definitionId {definitionId}.", definitionId);
+            throw;
+        }
     }
 
     public async Task ConfirmWriting(string definitionId, LastExecutionInfo lastExecutionInfo, CancellationToken cancellationToken)
     {
         this.logger.LogTrace("Entering to ConfirmWriting with definitionId={definitionId}.", definitionId);
 
-        using var session = this.documentStore.OpenAsyncSession();
-        var tsd = await session.LoadAsync<TimeSerieDefinition>(definitionId, cancellationToken);
-        tsd.LastExecutionInfo = lastExecutionInfo;
-        await session.SaveChangesAsync(cancellationToken);
+        try
+        {
+            using var session = this.documentStore.OpenAsyncSession();
+            var tsd = await session.LoadAsync<TimeSerieDefinition>(definitionId, cancellationToken);
+            tsd.LastExecutionInfo = lastExecutionInfo;
+            await session.SaveChangesAsync(cancellationToken);
 
-        this.logger.LogInformation("Confirm writing timeserie data into definition with id {definitionId}.", definitionId);
+            this.logger.LogInformation("Confirm writing timeserie data into definition with id {definitionId}.", definitionId);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in ConfirmWriting method with definitionId {definitionId}.", definitionId);
+            throw;
+        }
     }
 
     public async Task<IReadOnlyList<TimeSeriesItem>> ExecuteQuery(TimeSeriesQueryRequest request, CancellationToken cancellationToken)
     {
         this.logger.LogTrace("Entering to ExecuteQuery with DefinitionId {DefinitionId}.", request.DefinitionId);
 
-        Action<ITimePeriodBuilder> groupingAction = request.GroupByFunction switch //input is a string that represents some client input
+        try
         {
-            TimeSeriesGroupByFn.Seconds => (Action<ITimePeriodBuilder>)(builder => builder.Seconds(1)),
-            TimeSeriesGroupByFn.Minutes => (Action<ITimePeriodBuilder>)(builder => builder.Minutes(1)),
-            TimeSeriesGroupByFn.Hours => (Action<ITimePeriodBuilder>)(builder => builder.Hours(1)),
-            TimeSeriesGroupByFn.Days => (Action<ITimePeriodBuilder>)(builder => builder.Days(1)),
-            TimeSeriesGroupByFn.Months => (Action<ITimePeriodBuilder>)(builder => builder.Months(1)),
-            TimeSeriesGroupByFn.Quarters => (Action<ITimePeriodBuilder>)(builder => builder.Quarters(1)),
-            TimeSeriesGroupByFn.Years => (Action<ITimePeriodBuilder>)(builder => builder.Years(1)),
-            _ => throw new InvalidProgramException($"Enum value {request.GroupByFunction} is not supported.")
-        };
+            Action<ITimePeriodBuilder> groupingAction = request.GroupByFunction switch //input is a string that represents some client input
+            {
+                TimeSeriesGroupByFn.Seconds => (Action<ITimePeriodBuilder>)(builder => builder.Seconds(1)),
+                TimeSeriesGroupByFn.Minutes => (Action<ITimePeriodBuilder>)(builder => builder.Minutes(1)),
+                TimeSeriesGroupByFn.Hours => (Action<ITimePeriodBuilder>)(builder => builder.Hours(1)),
+                TimeSeriesGroupByFn.Days => (Action<ITimePeriodBuilder>)(builder => builder.Days(1)),
+                TimeSeriesGroupByFn.Months => (Action<ITimePeriodBuilder>)(builder => builder.Months(1)),
+                TimeSeriesGroupByFn.Quarters => (Action<ITimePeriodBuilder>)(builder => builder.Quarters(1)),
+                TimeSeriesGroupByFn.Years => (Action<ITimePeriodBuilder>)(builder => builder.Years(1)),
+                _ => throw new InvalidProgramException($"Enum value {request.GroupByFunction} is not supported.")
+            };
 
-        AgregateFunctionRuntime agregateRuntime = AgregateFunctionHelper.GetFunctions(request.AgregationFunction);
+            AgregateFunctionRuntime agregateRuntime = AgregateFunctionHelper.GetFunctions(request.AgregationFunction);
 
-        using var session = this.documentStore.OpenAsyncSession();
-        TimeSeriesAggregationResult ts = await session.Query<TimeSerieDefinition>()
-            .Where(t => t.Id == request.DefinitionId)
-            .Select(t => global::Raven.Client.Documents.Queries.RavenQuery.TimeSeries(t, TimeSeriesConstants.TsName, request.From, request.To)
-                .GroupBy(groupingAction)
-                 .Select(t => new
-                 {
-                     // TODO: fix slection
-                     Count = t.Count(),
-                     Sum = t.Sum(),
-                     Min = t.Min(),
-                     Max = t.Max(),
-                     Average = t.Average()
-                 })
-                 .ToList()
-                 )
-            .SingleAsync(cancellationToken);
+            using var session = this.documentStore.OpenAsyncSession();
+            TimeSeriesAggregationResult ts = await session.Query<TimeSerieDefinition>()
+                .Where(t => t.Id == request.DefinitionId)
+                .Select(t => global::Raven.Client.Documents.Queries.RavenQuery.TimeSeries(t, TimeSeriesConstants.TsName, request.From, request.To)
+                    .GroupBy(groupingAction)
+                     .Select(t => new
+                     {
+                         // TODO: fix selection
+                         Count = t.Count(),
+                         Sum = t.Sum(),
+                         Min = t.Min(),
+                         Max = t.Max(),
+                         Average = t.Average()
+                     })
+                     .ToList()
+                     )
+                .SingleAsync(cancellationToken);
 
-        return agregateRuntime.Mapper(ts); ;
+            return agregateRuntime.Mapper(ts);
+        }
+        catch (Exception ex)
+        {
+            this.logger.LogError(ex, "Unexcepted error in ExecuteQuery method with DefinitionId {DefinitionId}.", request.DefinitionId);
+            throw;
+        }
     }
 }
