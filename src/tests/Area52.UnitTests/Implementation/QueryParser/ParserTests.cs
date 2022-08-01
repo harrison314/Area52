9a2c1aa5881be40ead98e4f3a338f9412c9aa7a2
harrison314 <harrison314@azet.sk>
2022-08-01 17:59:49 +0200

    Add parser tests.

---
+++ src/tests/Area52.UnitTests/Implementation/QueryParser/ParserTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Area52.Services.Implementation.QueryParser;
+using Xunit.Abstractions;
+
+namespace Area52.UnitTests.Implementation.QueryParser;
+
+public class ParserTests
+{
+    private readonly ITestOutputHelper output;
+
+    public ParserTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+
+    [Theory]
+    [InlineData("Property is 'value'")]
+    [InlineData("Property == 'value'")]
+    [InlineData("Property is not 'value'")]
+    [InlineData("Property != 'value'")]
+    [InlineData("Property is eg 'value'")]
+    [InlineData("Property < 'value'")]
+    [InlineData("Property < 12.36")]
+    [InlineData("Property > 'value'")]
+    [InlineData("Property > 12.36")]
+    [InlineData("Property <= 'value'")]
+    [InlineData("Property <= 12.36")]
+    [InlineData("Property >= 'value'")]
+    [InlineData("Property >= 12.36")]
+    [InlineData("Property exists any")]
+    [InlineData("Property is not null")]
+    [InlineData("Property between 12 and 18.9")]
+    [InlineData("Property >= 12 and Property >= 18.9")]
+    [InlineData("'value'")]
+    [InlineData("Property startsWith 'value'")]
+    [InlineData("Property endsWith 'value'")]
+    [InlineData("Property is not 'value1' and Property is not 'value2'")]
+    [InlineData("Property is not 'value1' && Property is not 'value2'")]
+    [InlineData("Property is 'value1' or Property is 'value2'")]
+    [InlineData("Property is 'value1' || Property is 'value2'")]
+    [InlineData("RequestId is '0HMJ3E0UJPORG:00000007' and (Exception is not null or Level is 'Error')")]
+    public void Parser_SimpleParse_ParseExamples(string query)
+    {
+        IAstNode astNode = Parser.SimpleParse(query);
+        Assert.NotNull(astNode);
+
+        this.output.WriteLine(astNode.ToString());
+    }
+
+    [Theory]
+    [InlineData("Timestamp > datetime('2022-06-01')")]
+    [InlineData("Timestamp > -45.56")]
+    [InlineData("Applicaton search 'Area52'")]
+    public void Parser_SimpleParse_SpecialCases(string query)
+    {
+        IAstNode astNode = Parser.SimpleParse(query);
+        Assert.NotNull(astNode);
+
+        this.output.WriteLine(astNode.ToString());
+    }
+}
