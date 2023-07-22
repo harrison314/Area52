4e01a2b1ef4dca67a13eb631de1748d1ee53612c
harrison314 <harrison314@azet.sk>
2023-07-22 09:39:48 +0200

    UI enhancment.

---
+++ src/src/Area52/Shared/FormatingExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using Raven.Client.Documents.Commands;
+
+namespace Area52.Shared;
+
+internal static class FormatingExtensions
+{
+    private static readonly NumberFormatInfo customLongNumberInfo = CreateCustomLongNumberInfo();
+
+    public static string FormatLargeNumber(this long value)
+    {
+        return value.ToString("n", customLongNumberInfo);
+    }
+
+    private static NumberFormatInfo CreateCustomLongNumberInfo()
+    {
+        NumberFormatInfo nfi = (NumberFormatInfo)CultureInfo.InvariantCulture.NumberFormat.Clone();
+        nfi.NumberGroupSeparator = "\u2009";
+        nfi.NumberDecimalDigits = 0;
+
+        return nfi;
+    }
+}
